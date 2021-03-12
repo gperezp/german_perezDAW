beb0ef912ff9c3bc483c38ef17534ab836645612
gperez <gperezp10@iesalbarregas.es>
2021-03-12 16:27:23 +0100

    Se ha creado la rama ClaseNueva

--- german_perezDAW/src/german_perezDAW/NuevaClase.java
+++ german_perezDAW/src/german_perezDAW/NuevaClase.java
@@ -3,7 +3,8 @@ package german_perezDAW;
 public class NuevaClase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("En la rama. Se crea la clase nueva");
 
 	}
 
--- german_perezDAW/src/german_perezDAW/Principal.java
+++ german_perezDAW/src/german_perezDAW/Principal.java
@@ -3,7 +3,8 @@ package german_perezDAW;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Nueva línea clase Principal");
+		System.out.println("Segunda línea clase Principal");
 
 	}
 
